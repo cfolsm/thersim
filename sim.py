99d99ebfdcd6b9bab8ab161d6da3acd8a0cc4fc5
cfolsm <chris.folmar@ithermodyne.com>
2016-10-07 17:37:28 -0400

    second

--- sim.py
+++ sim.py
@@ -26,6 +26,10 @@ cond = 'ag'
 sink = 'air'
 debug = True 
 
+def paradd(v,n):
+    tmp = n/v
+    return 1/tmp
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # CNT film characteristics 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -38,11 +42,11 @@ alpha = {'brd_champ': 110.5e-6, 'char_champ': 60.0e-6, 'var': alpha_var }  # See
 film_sigma = 41000      # electrical conductivity of film [S/m]
 film_sigma_var = np.linspace(1000,500000,1000)
 film_thick = 1e-4  # film thickness [m]
-film_thick_var = np.linspace(1e-9,1e-2,10000000)
+film_thick_var = np.linspace(1e-9,1,10000000)
 T_h = 303            # Hot side temp [K]
 T_c= 293           # Cold side temp [K]
 sc = 'brd_champ'
-#film_thick = film_thick_var
+film_thick = film_thick_var
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Electrical resistivity constants
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -88,13 +92,13 @@ sink_area = 1
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #TF first
-tf_area = 1.56e-7   #area of individual n and p module [m]
-tf_area_var = np.linspace(1e-15,1e-5,10000000)
-tf_area = tf_area_var
+tf_area = 1.56e-4   #area of individual n and p module [m]
+tf_area_var = np.linspace(1e-10,1e-4,10000000)
+#tf_area = tf_area_var
 n_teg = sub_area/tf_area
 n_teg_var = sub_area/tf_area_var
 
-n_teg = n_teg_var
+#n_teg = n_teg_var
 
 print('number of n-p modules',n_teg)
 
@@ -115,7 +119,8 @@ cond_len = cond_width*2   # [m]
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-Th_cond = (cond_thick)/(kappa[cond]*cond_area)*n_teg		#Thermal resistance of conductor [K/W]
+Th_cond_i = (cond_thick)/(kappa[cond]*cond_area)  #Thermal resistance of conductor [K/W]
+Th_cond = paradd(Th_cond_i,n_teg)
 print('conductor area ', cond_area)
 print('conductor len ' , cond_len)
 print('conductor thermal resistance = ', Th_cond)
@@ -125,7 +130,8 @@ print('substrate thermal resistance = ', Th_sub)
 
 Th_sink = (sink_thick)/(kappa[sink]*sink_area)
 
-Th_Sl = (film_thick)/(kappa['film']*tf_area)*n_teg	#Thermal resistance of an individual n or p module [K/W]
+Th_Sl = (film_thick)/(kappa['film']*tf_area)
+Th_Sl = paradd(Th_Sl,n_teg)   #Thermal resistance of an individual n or p module [K/W]
 print('film area ', tf_area)
 print('film height ' , film_thick)
 
@@ -177,19 +183,19 @@ T_c = 700
 
 
 A = (Th_con + Th_Sl)*((alpha**2)*Th_con*Th_Sl*(T_h+T_c) + R_teg*(2*Th_con + Th_Sl))
-#print('A = ',A)
+print('A = ',A)
 
-B = ((Th_Sl + 2*Th_con)**2)*((alpha**2)*Th_con*Th_Sl*(T_h+T_c) + R_teg*(Th_Sl +2*Th_con))**2 - (alpha**4)*(Th_con**2)*(Th_Sl**3)*((T_h-T_c)**2)*(Th_Sl + 2*Th_con)
-#print('B = ',B)
+B = (((Th_Sl + 2*Th_con)**2)*((alpha**2)*Th_con*Th_Sl*(T_h+T_c) + R_teg*(Th_Sl +2*Th_con))**2) - (alpha**4)*(Th_con**2)*(Th_Sl**3)*((T_h-T_c)**2)*(Th_Sl + 2*Th_con)
+print('B = ',B)
 
-I_mpp = (A - np.sqrt(B))/((alpha**3)*(Th_con**2)*(Th_Sl**2)*(T_h-T_c))
+#I_mpp = (A - np.sqrt(B))/((alpha**3)*(Th_con**2)*(Th_Sl**2)*(T_h-T_c))
 #print('I_mpp = ',I_mpp)
 
-P_max = (I_mpp/((alpha**2)*(Th_con**2)*(I_mpp**2) - Th_Sl - 2*Th_con))*(((alpha**2)*Th_con*Th_Sl*(T_h+T_c) + R_teg*(Th_Sl + 2*Th_con))*I_mpp - alpha*Th_Sl*(T_h-T_c))
+#P_max = (I_mpp/((alpha**2)*(Th_con**2)*(I_mpp**2) - Th_Sl - 2*Th_con))*(((alpha**2)*Th_con*Th_Sl*(T_h+T_c) + R_teg*(Th_Sl + 2*Th_con))*I_mpp - alpha*Th_Sl*(T_h-T_c))
 
-#I_mpp = ((Th_Sl/(Th_Sl+2*Th_con))*alpha*(T_h-T_c))/(2*(R_teg+(((alpha**2)*Th_con*Th_Sl)/(Th_Sl+2*Th_con))*(T_h+T_c)))
+I_mpp = ((Th_Sl/(Th_Sl+2*Th_con))*alpha*(T_h-T_c))/(2*(R_teg+(((alpha**2)*Th_con*Th_Sl)/(Th_Sl+2*Th_con))*(T_h+T_c)))
 
-#P_max = (((Th_Sl/(Th_Sl+2*Th_con))*alpha*(T_h-T_c))**2)/(4*(R_teg+(((alpha**2)*Th_con*Th_Sl)/(Th_Sl+2*Th_con))*(T_h+T_c)))
+P_max = (((Th_Sl/(Th_Sl+2*Th_con))*alpha*(T_h-T_c))**2)/(4*(R_teg+(((alpha**2)*Th_con*Th_Sl)/(Th_Sl+2*Th_con))*(T_h+T_c)))
 
 V_teg = P_max/I_mpp
 
@@ -234,44 +240,39 @@ print('power = ' ,P_max)
 #P_teg = (1/R_ld)*pre_fact*(numerator/divisor)
 #print('max R_ld = ', min(P))
 
-'''
-cond_height = 100e-9
 
-v_m1 = alpha*n_teg*dT_TEAg/(1+2*(kappa['film']/kappa[cond])*(1e-9/film_height_var))
-v_m2 = alpha*n_teg*dT_TEAg/(1+2*(kappa['film']/kappa[cond])*(5e-9/film_height_var))
-v_m3 = alpha*n_teg*dT_TEAg/(1+2*(kappa['film']/kappa[cond])*(10e-9/film_height_var))
-v_m4 = alpha*n_teg*dT_TEAg/(1+2*(kappa['film']/kappa[cond])*(cond_height/film_height_var))
-v_m5 = alpha*n_teg*dT_TEAg/(1+2*(kappa['film']/kappa[cond])*(1000e-9/film_height_var))
 
-i_m1 = alpha*(1/n_teg)*dT_TEAg/(2*rho['film']*((2*rho[cond]/rho['film'])+1)*(1+2*(kappa['film']/kappa[cond])*(1e-9/film_height_var)))
-i_m2 = alpha*(1/n_teg)*dT_TEAg/(2*rho['film']*((2*rho[cond]/rho['film'])+1)*(1+2*(kappa['film']/kappa[cond])*(5e-9/film_height_var)))
-i_m3 = alpha*(1/n_teg)*dT_TEAg/(2*rho['film']*((2*rho[cond]/rho['film'])+1)*(1+2*(kappa['film']/kappa[cond])*(10e-9/film_height_var)))
-i_m4 = alpha*(1/n_teg)*dT_TEAg/(2*rho['film']*((2*rho[cond]/rho['film'])+1)*(1+2*(kappa['film']/kappa[cond])*(cond_height/film_height_var)))
-i_m5 = alpha*(1/n_teg)*dT_TEAg/(2*rho['film']*((2*rho[cond]/rho['film'])+1)*(1+2*(kappa['film']/kappa[cond])*(1000e-9/film_height_var)))
 
-#Max power achievable given heat exchange resistances
-#P_max = pow(dT,2)*nu_reduced/(4*T_hot*(Rth_external)*Area_module) # Units should be W/cm^2
-#Actual power given exact structure, assumes no electrical loses in Ag
-#P_act = Q_TE*nu/Area_module # W/cm^2
-
-p1 = v_m1*i_m1
-p2 = v_m2*i_m2
-p3 = v_m3*i_m3
-p4 = v_m4*i_m4
-p5 = v_m5*5
-
-'''
 fig, ax = plt.subplots()
-#ax.plot(xf, 2.0/L * np.abs(A[:L/2]))
-#ax.plot(film_height_var,p1)
-#ax.plot(film_height_var,p2)
-#ax.plot(film_height_var,p3)
-#ax.plot(film_height_var,p4)
-ax.plot(n_teg,P_max)
+
+
+ax.plot(film_thick, P_max)
 ax.set_xlabel('film thickness')
 ax.set_ylabel('Power output [W]')
 ax.set_xscale('log')
 
 plt.show()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# unused code
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`:w
+
+#V_teg = (alpha[sc]*T_h*R_ld - alpha[sc]*T_c*R_ld)/(R_teg+R_ld) #no thermal contact resitance
+
+#V_teg = (2*3**(1/3)*alpha**4*Th_con**3*(T_c+T_h)*R_teg*R_ld**2*Th_Sl**2 + 2*3**(1/3)*alpha**2*Th_con**2*R_teg*R_ld**2*(R_teg+R_ld)*Th_Sl*(2*Th_con+Th_Sl) - 2**(1/3)*((9*alpha**5*Th_con**4*(T_h - T_c)*R_teg**2*R_ld**3*Th_Sl**3 + 3**(1/2)*(alpha**6*Th_con**6*R_teg**3*R_ld**6*Th_Sl**3*(27*alpha**4*Th_con**2*(T_c-T_h)**2*R_teg*Th_Sl**3 - 4*(2*Th_con*(R_teg+R_ld) + alpha**2*Th_con*(T_c+T_h)*Th_Sl + (R_teg+R_ld)*Th_Sl)**3)))**(1/2))**(2/3))/((6**(2/3)*alpha**2*Th_con**2*R_teg*Th_Sl*(9*alpha**5*Th_con**4*(T_h-T_c)*R_teg**2*R_ld**3*Th_Sl**3 + 3**(1/2)*(alpha**6*Th_con**6*R_teg**3*R_ld**6*Th_Sl**3*(27*alpha**4*Th_con**2*(T_c-T_h)**2*R_teg*Th_Sl**3 - 4*(2*Th_con*(R_teg+R_ld) + alpha**2*Th_con*(T_c+T_h)*Th_Sl + (R_teg+R_ld)*Th_Sl)**3)))**(1/2))**(1/3))
+
+#V_teg = -alpha*T_c*R_ld**3*Th_Sl + alpha*T_h*R_ld**3*Th_Sl + (-2*Th_con*R_teg*R_ld**2 - 2*Th_con*R_ld**3 - alpha**2*T_c*Th_con*R_ld**2*Th_Sl - alpha**2*Th_con*T_h*R_ld**2*Th_Sl - R_teg*R_ld**2*Th_Sl - R_ld**3*Th_Sl) + alpha**2*Th_con**2*R_teg*Th_Sl
+
+#V_teg = *R_ld*((alpha[sc]*n_teg - 1)*T_c + (alpha[sc]*n_teg + 1)*T_h)**(1/3)/((alpha[sc]*n_teg)**(1/3)*(Th_con['const']*n_teg)**(2/3)*(R_teg*n_teg)**(1/3))/10
+
+#V_teg = math.sqrt(pre_fact)*R_ld*((alpha[sc]*n_teg - 1)*T_c + (alpha[sc]*n_teg + 1)*T_h)**(1/3)/((alpha[sc]*n_teg)**(1/3)*(Th_con['const']*n_teg)**(2/3)*(R_teg*n_teg)**(1/3))/10
+
+#neg = alpha[sc]**2*T_c*R_ld**2*Th_Sl*Th_con+alpha[sc]**2*T_c*R_ld**2*Th_Sl*Th_con - (R_teg*R_ld**2*Th_Sl + 2*R_teg*R_ld**2+Th_Sl*R_ld**3+2*R_ld**3*Th_con)
+
+#V_teg = (1/(3*(2**(1/2)*alpha[sc]**2*Th_Sl*Th_con**2)))*((27*alpha[sc]**5*T_c*R_ld**2*Th_Sl**3*Th_con**4 - 27*alpha[sc]**5*T_h*R_ld**2*Th_Sl**3*Th_con**4 + ((27*alpha[sc]**5*T_c*R_ld**2*Th_Sl**3*Th_con**4 + 27*alpha[sc]**5*T_h*R_ld**2*Th_Sl**3*Th_con**4)**2 + 108*alpha[sc]**6*Th_Sl**3*Th_con**6*(-alpha[sc]**2*T_c*R_ld*Th_Sl*Th_con + alpha[sc]**2*T_h*R_ld*Th_Sl*Th_con + R_teg*R_ld*Th_Sl + 2*R_teg*R_ld*Th_con + R_ld**2*Th_con)**3)**(1/2)))**(1/3)- ( 2**(1/2)*(alpha[sc]**2*(-T_c)*R_ld*Th_Sl*Th_con - alpha[sc]**2*T_h*R_ld*Th_Sl*Th_con - R_teg*R_ld*Th_Sl - 2*R_teg*R_ld*Th_con - R_ld**2*Th_Sl - 2*R_ld**2*Th_con))/((27*alpha[sc]**5*T_c*R_ld**2*Th_Sl**3*Th_con**4 - 27*alpha[sc]**5*T_h*R_ld**2*Th_Sl**3*Th_con**4 + ((27*alpha[sc]**5*T_c*R_ld**2*Th_Sl**3*Th_con**4 + 27*alpha[sc]**5*T_h*R_ld**2*Th_Sl**3*Th_con**4)**2 + 108*alpha[sc]**6*Th_Sl**3*Th_con**6*(alpha[sc]**2*T_c*R_ld*Th_Sl*Th_con + alpha[sc]**2*T_h*R_ld*Th_Sl*Th_con + R_teg*R_ld*Th_Sl + 2*R_teg*R_ld*Th_con + R_ld**2*Th_Sl + 2*R_ld**2*Th_con)**3)**(1/2))**(1/3))
+
+#print(V_teg)
+#V_teg = V_teg.real
+#V_teg = ((108*alpha[sc]**5*T_c*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 + 108*alpha[sc]**5*T_c*R_teg*R_ld**4*Th_Sl**3*Th_con**4+27*alpha[sc]**5*T_c*R_ld**5*Th_Sl**3*Th_con**4 - 108*alpha[sc]**5*T_h*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 - 108*alpha[sc]**5*T_h*R_teg*R_ld**4*Th_Sl**3*Th_con**4 - 27*alpha[sc]**5*T_h*R_ld**5*Th_Sl**3*Th_con**4 + ((108*alpha[sc]**5*T_c*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 + 108*alpha[sc]**5*T_c*R_teg*R_ld**4*Th_Sl**3*Th_con**4 + 27*alpha[sc]**5*T_c*R_ld**5*Th_Sl**3*Th_con**4 - 108*alpha[sc]**5*T_h*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 - 108*alpha[sc]**5*T_h*R_teg*R_ld**4*Th_Sl**3*Th_con**4 - 27*alpha[sc]**5*T_h*R_ld**5*Th_Sl**3*Th_con**4)**2 + 108*(2*alpha[sc]**2*R_teg*Th_Sl*Th_con**2 + alpha[sc]**2*R_ld*Th_Sl*Th_con**2)**3*(alpha[sc]**2*T_c*R_ld**2*Th_Sl*Th_con + alpha[sc]**2*T_h*R_ld**2*Th_Sl*Th_con - R_teg*R_ld**2*Th_Sl - 2*R_teg*R_ld**2*Th_con + R_ld**3*(-Th_Sl) - 2*R_ld**3*Th_con)**3)**(1/2))**(1/3))/(3*2**(1/3)*(2*alpha[sc]**2*R_teg*Th_Sl*Th_con**2 + alpha[sc]**2*R_ld*Th_Sl*Th_con**2)) - (2**(1/3)*(alpha[sc]**2*T_c*R_ld**2*Th_Sl*Th_con + alpha[sc]**2*T_h*R_ld**2*Th_Sl*Th_con - R_teg*R_ld**2*Th_Sl - 2*R_teg*R_ld**2*Th_con + R_ld**3*(-Th_Sl) - 2*R_ld**3*Th_con))/((108*alpha[sc]**5*T_c*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 + 108*alpha[sc]*T_c*R_teg*R_ld**4*Th_Sl**3*Th_con**4 + 27*alpha[sc]**5*T_c*R_ld**5*Th_Sl**3*Th_con**4 + ((108*alpha[sc]**5*T_c*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 + 108*alpha[sc]**5*T_c*R_teg*R_ld**4*Th_Sl**3*Th_con**4 + 27*alpha[sc]**5*T_c*R_ld**5*Th_Sl**3*Th_con**4 -108*alpha[sc]**5*T_h*R_teg**2*R_ld**3*Th_Sl**3*Th_con**4 - 108*alpha[sc]**5*T_h*R_teg*R_ld**4*Th_Sl**3*Th_con**4 - 27*alpha[sc]**5*T_h*R_ld**5*Th_Sl**3*Th_con**4)**2 + 108*(2*alpha[sc]**2*R_teg*Th_Sl*Th_con**2 + alpha[sc]**2*R_ld*Th_Sl*Th_con**2)**3*(alpha[sc]*T_c*R_ld**2*Th_Sl*Th_con + alpha[sc]**2*T_h*R_ld**2*Th_Sl*Th_con - R_teg*R_ld**2*Th_Sl - 2*R_teg*R_ld**2*Th_con + R_ld**3*(-Th_Sl) - 2*R_ld**3*Th_con)**3)**(1/2))**(1/3))
 
 
